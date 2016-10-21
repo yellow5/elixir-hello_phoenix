a221151d66a05ba964d8e2c8aa13acca8adb50af
Cameron Dykes <yellow5c@gmail.com>
2016-10-21 16:50:06 -0500

    Add text view template for main page

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,10 +1,11 @@
 defmodule HelloPhoenix.PageController do
   use HelloPhoenix.Web, :controller
 
-  def index(conn, _params) do
+  def index(conn, params) do
     conn
+    |> assign(:message, params["message"])
     |> put_flash(:info, "Welcome to Phoenix, from flash info!")
     |> put_flash(:error, "Let's pretend we have an error.")
-    |> render("index.html")
+    |> render(:index)
   end
 end
