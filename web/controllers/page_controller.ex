a4aa65fe932937aa1bc2dbe6e442c1d9aea08679
Cameron Dykes <yellow5c@gmail.com>
2016-10-21 16:13:42 -0500

    Add sample flash info + error messages to PageController.index/2

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,9 @@ defmodule HelloPhoenix.PageController do
   use HelloPhoenix.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    conn
+    |> put_flash(:info, "Welcome to Phoenix, from flash info!")
+    |> put_flash(:error, "Let's pretend we have an error.")
+    |> render("index.html")
   end
 end
