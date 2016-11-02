72dfa0fea2684fca10189cbd1e4f665e553efdde
Cameron Dykes <yellow5c@gmail.com>
2016-11-02 15:46:16 -0500

    Improve @valid_attrs map for user tests

--- test/controllers/user_controller_test.exs
+++ test/controllers/user_controller_test.exs
@@ -2,7 +2,7 @@ defmodule HelloPhoenix.UserControllerTest do
   use HelloPhoenix.ConnCase
 
   alias HelloPhoenix.User
-  @valid_attrs %{bio: "some content", email: "some content", name: "some content", number_of_pets: 42}
+  @valid_attrs %{bio: "Life info.", email: "email@example.com", name: "User Name", number_of_pets: 4}
   @invalid_attrs %{}
 
   test "lists all entries on index", %{conn: conn} do
--- test/models/user_test.exs
+++ test/models/user_test.exs
@@ -3,7 +3,7 @@ defmodule HelloPhoenix.UserTest do
 
   alias HelloPhoenix.User
 
-  @valid_attrs %{bio: "some content", email: "some content", name: "some content", number_of_pets: 42}
+  @valid_attrs %{bio: "Life info.", email: "email@example.com", name: "User Name", number_of_pets: 4}
   @invalid_attrs %{}
 
   test "changeset with valid attributes" do
