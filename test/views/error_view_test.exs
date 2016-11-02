7d7be9a7dbc9f6fcef6c940c02f846b60bd234be
Cameron Dykes <yellow5c@gmail.com>
2016-11-02 15:13:20 -0500

    Update the 404 view test

--- test/views/error_view_test.exs
+++ test/views/error_view_test.exs
@@ -5,8 +5,8 @@ defmodule HelloPhoenix.ErrorViewTest do
   import Phoenix.View
 
   test "renders 404.html" do
-    assert render_to_string(HelloPhoenix.ErrorView, "404.html", []) ==
-           "Page not found"
+    assert render_to_string(HelloPhoenix.ErrorView, "404.html", []) =~
+           "Sorry, the page you are looking for does not exist."
   end
 
   test "render 500.html" do
