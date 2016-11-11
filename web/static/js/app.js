f3d35b39d2f2797fda9c3dcac71528ace55f966b
Cameron Dykes <yellow5c@gmail.com>
2016-11-11 14:34:13 -0600

    Add a basic Hello to the primary app JS and run it in the main layout

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -19,3 +19,9 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 import socket from "./socket"
+
+export var App = {
+  run: function() {
+    console.log("Hello!")
+  }
+}
